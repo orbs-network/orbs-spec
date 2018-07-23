4507e6582cc830ff381db18bb039f50c79fc19fa
Tal Kol <talkol@gmail.com>
2018-07-23 14:36:40 +0300

    Added list of consensus algorithms

---
+++ types/go/protocol/consensus/all.mb.go
@@ -0,0 +1,29 @@
+// AUTO GENERATED FILE (by membufc proto compiler v0.0.18)
+package consensus
+
+import (
+)
+
+/////////////////////////////////////////////////////////////////////////////
+// enums
+
+type ConsensusAlgoType uint16
+
+const (
+	CONSENSUS_ALGO_TYPE_RESERVED ConsensusAlgoType = 0
+	CONSENSUS_ALGO_TYPE_BENCHMARK_CONSENSUS ConsensusAlgoType = 1
+	CONSENSUS_ALGO_TYPE_LEAN_HELIX ConsensusAlgoType = 2
+)
+
+func (n ConsensusAlgoType) String() string {
+	switch n {
+	case CONSENSUS_ALGO_TYPE_RESERVED:
+		return "CONSENSUS_ALGO_TYPE_RESERVED"
+	case CONSENSUS_ALGO_TYPE_BENCHMARK_CONSENSUS:
+		return "CONSENSUS_ALGO_TYPE_BENCHMARK_CONSENSUS"
+	case CONSENSUS_ALGO_TYPE_LEAN_HELIX:
+		return "CONSENSUS_ALGO_TYPE_LEAN_HELIX"
+	}
+	return "UNKNOWN"
+}
+
