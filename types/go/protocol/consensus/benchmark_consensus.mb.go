ecd9cc847da320abb92828a75baffb06b2e33e0e
Tal Kol <talkol@gmail.com>
2018-12-19 23:56:17 +0200

    Changed BenchmarkConsensus block proof to resemble lean helix

--- types/go/protocol/consensus/benchmark_consensus.mb.go
+++ types/go/protocol/consensus/benchmark_consensus.mb.go
@@ -14,7 +14,9 @@ import (
 // reader
 
 type BenchmarkConsensusBlockProof struct {
-	// Sender BenchmarkConsensusSenderSignature
+	// BlockRef BlockRef
+	// Nodes []BenchmarkConsensusSenderSignature
+	// Placeholder []byte
 
 	// internal
 	// implements membuffers.Message
@@ -25,10 +27,10 @@ func (x *BenchmarkConsensusBlockProof) String() string {
 	if x == nil {
 		return "<nil>"
 	}
-	return fmt.Sprintf("{Sender:%s,}", x.StringSender())
+	return fmt.Sprintf("{BlockRef:%s,Nodes:%s,Placeholder:%s,}", x.StringBlockRef(), x.StringNodes(), x.StringPlaceholder())
 }
 
-var _BenchmarkConsensusBlockProof_Scheme = []membuffers.FieldType{membuffers.TypeMessage}
+var _BenchmarkConsensusBlockProof_Scheme = []membuffers.FieldType{membuffers.TypeMessage, membuffers.TypeMessageArray, membuffers.TypeBytes}
 var _BenchmarkConsensusBlockProof_Unions = [][]membuffers.FieldType{}
 
 func BenchmarkConsensusBlockProofReader(buf []byte) *BenchmarkConsensusBlockProof {
@@ -55,32 +57,96 @@ func (x *BenchmarkConsensusBlockProof) Equal(y *BenchmarkConsensusBlockProof) bo
 	return bytes.Equal(x.Raw(), y.Raw())
 }
 
-func (x *BenchmarkConsensusBlockProof) Sender() *BenchmarkConsensusSenderSignature {
+func (x *BenchmarkConsensusBlockProof) BlockRef() *BlockRef {
 	b, s := x._message.GetMessage(0)
-	return BenchmarkConsensusSenderSignatureReader(b[:s])
+	return BlockRefReader(b[:s])
 }
 
-func (x *BenchmarkConsensusBlockProof) RawSender() []byte {
+func (x *BenchmarkConsensusBlockProof) RawBlockRef() []byte {
 	return x._message.RawBufferForField(0, 0)
 }
 
-func (x *BenchmarkConsensusBlockProof) RawSenderWithHeader() []byte {
+func (x *BenchmarkConsensusBlockProof) RawBlockRefWithHeader() []byte {
 	return x._message.RawBufferWithHeaderForField(0, 0)
 }
 
-func (x *BenchmarkConsensusBlockProof) StringSender() string {
-	return x.Sender().String()
+func (x *BenchmarkConsensusBlockProof) StringBlockRef() string {
+	return x.BlockRef().String()
+}
+
+func (x *BenchmarkConsensusBlockProof) NodesIterator() *BenchmarkConsensusBlockProofNodesIterator {
+	return &BenchmarkConsensusBlockProofNodesIterator{iterator: x._message.GetMessageArrayIterator(1)}
+}
+
+type BenchmarkConsensusBlockProofNodesIterator struct {
+	iterator *membuffers.Iterator
+}
+
+func (i *BenchmarkConsensusBlockProofNodesIterator) HasNext() bool {
+	return i.iterator.HasNext()
+}
+
+func (i *BenchmarkConsensusBlockProofNodesIterator) NextNodes() *BenchmarkConsensusSenderSignature {
+	b, s := i.iterator.NextMessage()
+	return BenchmarkConsensusSenderSignatureReader(b[:s])
+}
+
+func (x *BenchmarkConsensusBlockProof) RawNodesArray() []byte {
+	return x._message.RawBufferForField(1, 0)
+}
+
+func (x *BenchmarkConsensusBlockProof) RawNodesArrayWithHeader() []byte {
+	return x._message.RawBufferWithHeaderForField(1, 0)
+}
+
+func (x *BenchmarkConsensusBlockProof) StringNodes() (res string) {
+	res = "["
+	for i := x.NodesIterator(); i.HasNext(); {
+		res += i.NextNodes().String() + ","
+	}
+	res += "]"
+	return
+}
+
+func (x *BenchmarkConsensusBlockProof) Placeholder() []byte {
+	return x._message.GetBytes(2)
+}
+
+func (x *BenchmarkConsensusBlockProof) RawPlaceholder() []byte {
+	return x._message.RawBufferForField(2, 0)
+}
+
+func (x *BenchmarkConsensusBlockProof) RawPlaceholderWithHeader() []byte {
+	return x._message.RawBufferWithHeaderForField(2, 0)
+}
+
+func (x *BenchmarkConsensusBlockProof) MutatePlaceholder(v []byte) error {
+	return x._message.SetBytes(2, v)
+}
+
+func (x *BenchmarkConsensusBlockProof) StringPlaceholder() string {
+	return fmt.Sprintf("%x", x.Placeholder())
 }
 
 // builder
 
 type BenchmarkConsensusBlockProofBuilder struct {
-	Sender *BenchmarkConsensusSenderSignatureBuilder
+	BlockRef    *BlockRefBuilder
+	Nodes       []*BenchmarkConsensusSenderSignatureBuilder
+	Placeholder []byte
 
 	// internal
 	// implements membuffers.Builder
 	_builder               membuffers.InternalBuilder
 	_overrideWithRawBuffer []byte
+}
+
+func (w *BenchmarkConsensusBlockProofBuilder) arrayOfNodes() []membuffers.MessageWriter {
+	res := make([]membuffers.MessageWriter, len(w.Nodes))
+	for i, v := range w.Nodes {
+		res[i] = v
+	}
+	return res
 }
 
 func (w *BenchmarkConsensusBlockProofBuilder) Write(buf []byte) (err error) {
@@ -98,10 +164,15 @@ func (w *BenchmarkConsensusBlockProofBuilder) Write(buf []byte) (err error) {
 		return w._builder.WriteOverrideWithRawBuffer(buf, w._overrideWithRawBuffer)
 	}
 	w._builder.Reset()
-	err = w._builder.WriteMessage(buf, w.Sender)
+	err = w._builder.WriteMessage(buf, w.BlockRef)
 	if err != nil {
 		return
 	}
+	err = w._builder.WriteMessageArray(buf, w.arrayOfNodes())
+	if err != nil {
+		return
+	}
+	w._builder.WriteBytes(buf, w.Placeholder)
 	return nil
 }
 
@@ -115,10 +186,15 @@ func (w *BenchmarkConsensusBlockProofBuilder) HexDump(prefix string, offsetFromS
 		}
 	}()
 	w._builder.Reset()
-	err = w._builder.HexDumpMessage(prefix, offsetFromStart, "BenchmarkConsensusBlockProof.Sender", w.Sender)
+	err = w._builder.HexDumpMessage(prefix, offsetFromStart, "BenchmarkConsensusBlockProof.BlockRef", w.BlockRef)
 	if err != nil {
 		return
 	}
+	err = w._builder.HexDumpMessageArray(prefix, offsetFromStart, "BenchmarkConsensusBlockProof.Nodes", w.arrayOfNodes())
+	if err != nil {
+		return
+	}
+	w._builder.HexDumpBytes(prefix, offsetFromStart, "BenchmarkConsensusBlockProof.Placeholder", w.Placeholder)
 	return nil
 }
 
@@ -312,6 +388,204 @@ func BenchmarkConsensusSenderSignatureBuilderFromRaw(raw []byte) *BenchmarkConse
 }
 
 /////////////////////////////////////////////////////////////////////////////
+// message BlockRef
+
+// reader
+
+type BlockRef struct {
+	// MessageType BenchmarkConsensusPlaceholder
+	// BlockHeight primitives.BlockHeight
+	// View uint64
+	// BlockHash primitives.Sha256
+
+	// internal
+	// implements membuffers.Message
+	_message membuffers.InternalMessage
+}
+
+func (x *BlockRef) String() string {
+	if x == nil {
+		return "<nil>"
+	}
+	return fmt.Sprintf("{MessageType:%s,BlockHeight:%s,View:%s,BlockHash:%s,}", x.StringMessageType(), x.StringBlockHeight(), x.StringView(), x.StringBlockHash())
+}
+
+var _BlockRef_Scheme = []membuffers.FieldType{membuffers.TypeUint16, membuffers.TypeUint64, membuffers.TypeUint64, membuffers.TypeBytes}
+var _BlockRef_Unions = [][]membuffers.FieldType{}
+
+func BlockRefReader(buf []byte) *BlockRef {
+	x := &BlockRef{}
+	x._message.Init(buf, membuffers.Offset(len(buf)), _BlockRef_Scheme, _BlockRef_Unions)
+	return x
+}
+
+func (x *BlockRef) IsValid() bool {
+	return x._message.IsValid()
+}
+
+func (x *BlockRef) Raw() []byte {
+	return x._message.RawBuffer()
+}
+
+func (x *BlockRef) Equal(y *BlockRef) bool {
+	if x == nil && y == nil {
+		return true
+	}
+	if x == nil || y == nil {
+		return false
+	}
+	return bytes.Equal(x.Raw(), y.Raw())
+}
+
+func (x *BlockRef) MessageType() BenchmarkConsensusPlaceholder {
+	return BenchmarkConsensusPlaceholder(x._message.GetUint16(0))
+}
+
+func (x *BlockRef) RawMessageType() []byte {
+	return x._message.RawBufferForField(0, 0)
+}
+
+func (x *BlockRef) MutateMessageType(v BenchmarkConsensusPlaceholder) error {
+	return x._message.SetUint16(0, uint16(v))
+}
+
+func (x *BlockRef) StringMessageType() string {
+	return x.MessageType().String()
+}
+
+func (x *BlockRef) BlockHeight() primitives.BlockHeight {
+	return primitives.BlockHeight(x._message.GetUint64(1))
+}
+
+func (x *BlockRef) RawBlockHeight() []byte {
+	return x._message.RawBufferForField(1, 0)
+}
+
+func (x *BlockRef) MutateBlockHeight(v primitives.BlockHeight) error {
+	return x._message.SetUint64(1, uint64(v))
+}
+
+func (x *BlockRef) StringBlockHeight() string {
+	return fmt.Sprintf("%s", x.BlockHeight())
+}
+
+func (x *BlockRef) View() uint64 {
+	return x._message.GetUint64(2)
+}
+
+func (x *BlockRef) RawView() []byte {
+	return x._message.RawBufferForField(2, 0)
+}
+
+func (x *BlockRef) MutateView(v uint64) error {
+	return x._message.SetUint64(2, v)
+}
+
+func (x *BlockRef) StringView() string {
+	return fmt.Sprintf("%x", x.View())
+}
+
+func (x *BlockRef) BlockHash() primitives.Sha256 {
+	return primitives.Sha256(x._message.GetBytes(3))
+}
+
+func (x *BlockRef) RawBlockHash() []byte {
+	return x._message.RawBufferForField(3, 0)
+}
+
+func (x *BlockRef) RawBlockHashWithHeader() []byte {
+	return x._message.RawBufferWithHeaderForField(3, 0)
+}
+
+func (x *BlockRef) MutateBlockHash(v primitives.Sha256) error {
+	return x._message.SetBytes(3, []byte(v))
+}
+
+func (x *BlockRef) StringBlockHash() string {
+	return fmt.Sprintf("%s", x.BlockHash())
+}
+
+// builder
+
+type BlockRefBuilder struct {
+	MessageType BenchmarkConsensusPlaceholder
+	BlockHeight primitives.BlockHeight
+	View        uint64
+	BlockHash   primitives.Sha256
+
+	// internal
+	// implements membuffers.Builder
+	_builder               membuffers.InternalBuilder
+	_overrideWithRawBuffer []byte
+}
+
+func (w *BlockRefBuilder) Write(buf []byte) (err error) {
+	if w == nil {
+		return
+	}
+	w._builder.NotifyBuildStart()
+	defer w._builder.NotifyBuildEnd()
+	defer func() {
+		if r := recover(); r != nil {
+			err = &membuffers.ErrBufferOverrun{}
+		}
+	}()
+	if w._overrideWithRawBuffer != nil {
+		return w._builder.WriteOverrideWithRawBuffer(buf, w._overrideWithRawBuffer)
+	}
+	w._builder.Reset()
+	w._builder.WriteUint16(buf, uint16(w.MessageType))
+	w._builder.WriteUint64(buf, uint64(w.BlockHeight))
+	w._builder.WriteUint64(buf, w.View)
+	w._builder.WriteBytes(buf, []byte(w.BlockHash))
+	return nil
+}
+
+func (w *BlockRefBuilder) HexDump(prefix string, offsetFromStart membuffers.Offset) (err error) {
+	if w == nil {
+		return
+	}
+	defer func() {
+		if r := recover(); r != nil {
+			err = &membuffers.ErrBufferOverrun{}
+		}
+	}()
+	w._builder.Reset()
+	w._builder.HexDumpUint16(prefix, offsetFromStart, "BlockRef.MessageType", uint16(w.MessageType))
+	w._builder.HexDumpUint64(prefix, offsetFromStart, "BlockRef.BlockHeight", uint64(w.BlockHeight))
+	w._builder.HexDumpUint64(prefix, offsetFromStart, "BlockRef.View", w.View)
+	w._builder.HexDumpBytes(prefix, offsetFromStart, "BlockRef.BlockHash", []byte(w.BlockHash))
+	return nil
+}
+
+func (w *BlockRefBuilder) GetSize() membuffers.Offset {
+	if w == nil {
+		return 0
+	}
+	return w._builder.GetSize()
+}
+
+func (w *BlockRefBuilder) CalcRequiredSize() membuffers.Offset {
+	if w == nil {
+		return 0
+	}
+	w.Write(nil)
+	return w._builder.GetSize()
+}
+
+func (w *BlockRefBuilder) Build() *BlockRef {
+	buf := make([]byte, w.CalcRequiredSize())
+	if w.Write(buf) != nil {
+		return nil
+	}
+	return BlockRefReader(buf)
+}
+
+func BlockRefBuilderFromRaw(raw []byte) *BlockRefBuilder {
+	return &BlockRefBuilder{_overrideWithRawBuffer: raw}
+}
+
+/////////////////////////////////////////////////////////////////////////////
 // enums
 
 type BenchmarkConsensusMessageType uint16
@@ -330,6 +604,29 @@ func (n BenchmarkConsensusMessageType) String() string {
 		return "BENCHMARK_CONSENSUS_COMMIT"
 	case BENCHMARK_CONSENSUS_COMMITTED:
 		return "BENCHMARK_CONSENSUS_COMMITTED"
+	}
+	return "UNKNOWN"
+}
+
+type BenchmarkConsensusPlaceholder uint16
+
+const (
+	BENCHMARK_CONSENSUS_INVALID_0 BenchmarkConsensusPlaceholder = 0
+	BENCHMARK_CONSENSUS_INVALID_1 BenchmarkConsensusPlaceholder = 1
+	BENCHMARK_CONSENSUS_INVALID_2 BenchmarkConsensusPlaceholder = 2
+	BENCHMARK_CONSENSUS_VALID     BenchmarkConsensusPlaceholder = 3
+)
+
+func (n BenchmarkConsensusPlaceholder) String() string {
+	switch n {
+	case BENCHMARK_CONSENSUS_INVALID_0:
+		return "BENCHMARK_CONSENSUS_INVALID_0"
+	case BENCHMARK_CONSENSUS_INVALID_1:
+		return "BENCHMARK_CONSENSUS_INVALID_1"
+	case BENCHMARK_CONSENSUS_INVALID_2:
+		return "BENCHMARK_CONSENSUS_INVALID_2"
+	case BENCHMARK_CONSENSUS_VALID:
+		return "BENCHMARK_CONSENSUS_VALID"
 	}
 	return "UNKNOWN"
 }
