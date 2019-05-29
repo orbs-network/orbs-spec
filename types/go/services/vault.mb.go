ecf5ae4d73a9c4acec3347b956f6f36fb6281bde
OdedW <odedw@orbs.com>
2019-05-29 15:00:24 +0300

    vault v1

---
+++ types/go/services/vault.mb.go
@@ -0,0 +1,53 @@
+// AUTO GENERATED FILE (by membufc proto compiler v0.0.21)
+package services
+
+import (
+	"context"
+	"fmt"
+	"github.com/orbs-network/orbs-spec/types/go/primitives"
+)
+
+/////////////////////////////////////////////////////////////////////////////
+// service Vault
+
+type Vault interface {
+	NodeSign(ctx context.Context, input *NodeSignInput) (*NodeSignOutput, error)
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// message NodeSignInput (non serializable)
+
+type NodeSignInput struct {
+	Data []byte
+}
+
+func (x *NodeSignInput) String() string {
+	if x == nil {
+		return "<nil>"
+	}
+	return fmt.Sprintf("{Data:%s,}", x.StringData())
+}
+
+func (x *NodeSignInput) StringData() (res string) {
+	res = fmt.Sprintf("%x", x.Data)
+	return
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// message NodeSignOutput (non serializable)
+
+type NodeSignOutput struct {
+	Signatrue primitives.EcdsaSecp256K1Sig
+}
+
+func (x *NodeSignOutput) String() string {
+	if x == nil {
+		return "<nil>"
+	}
+	return fmt.Sprintf("{Signatrue:%s,}", x.StringSignatrue())
+}
+
+func (x *NodeSignOutput) StringSignatrue() (res string) {
+	res = fmt.Sprintf("%s", x.Signatrue)
+	return
+}
